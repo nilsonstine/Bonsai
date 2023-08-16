6319d62f502cdab2afdac2d932c8dc839c94c66c
nilsonstine <21311514+nilsonstine@users.noreply.github.com>
2023-08-16 19:08:40 +0200

    new lib with patches

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,36 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_DRV2605.h>
+#include <drv2605.h>
 
-Adafruit_DRV2605 drv;
+DRV2605 haptic;
 
 void setup()
 {
     Serial.begin(115200);
-
-    if (!drv.begin())
+    /* Software I2C = false, Verbose = true */
+    if (haptic.init(false, true) != 0)
     {
-        Serial.println("Failed to find DRV2605, check wiring!");
-        while (1)
-            ;
+        Serial.println("init failed!");
     }
-
-    drv.selectLibrary(1); // Choose the LRA library
-
-    drv.setMode(DRV2605_MODE_REALTIME); // Set mode to real-time control
-
-    Serial.println("Testing all haptic effects...");
-
-    Serial.println("Testing complete.");
+    if (haptic.drv2605_AutoCal() != 0)
+    {
+        Serial.println("auto calibration failed!");
+    }
+    delay(2000);
 }
 
 void loop()
 {
-    drv.setWaveform(0, 25); // play effect
-    drv.setWaveform(1, 0);      // end waveform
+    
+        unsigned char i;
+        for(i=1;i<124;i++)
+        {
+            Serial.print("Effect No: ");
+            Serial.println(i);
 
-    // play the effect!
-    drv.go();
-
-    // wait a bit
-    delay(500);
+            haptic.drv2605_Play_Waveform(i);
+            delay(2000);
+        }
+    
+    //haptic.drv2605_Play_Waveform(118);
+    delay(2000);
 }
